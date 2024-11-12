ceeb0902002d84267401ae6ee9fc240a51a432f4
SergioAle210 <ore221122@uvg.edu.gt>
2024-11-12 11:21:58 -0600

    FEAT: Se probó con valores de 10 dígitos

--- Lab8-2.py
+++ Lab8-2.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Definir los valores de a y b
-a = 50000
-b = 30000
+a = 8000000000
+b = 4000000000
 
 # Número de repeticiones para la suma en cada experimento
 N = 10000
